6cdad32b27b49c27326b6734fb773392274ff4c4
andyarmor <aoarmor25@students.bmchs.org>
2022-08-18 15:56:00 -0500

    cm hello from home

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,9 @@
+from turtle import home
+
+
 print("hello world")
 print("hows it going?")
 print("going pretty good for tuesday")
 print("askjdfhaskjdfhsk")
 print("hello from school")
+print("hello from home")
